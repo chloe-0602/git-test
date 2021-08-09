f021e70635fa4474c6ee1eb2e4dc11ce3aa66fe9
chloe <chloe@atguigu.com>
2021-08-10 07:27:22 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Chloe
+ * @create 2021-08-10 7:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
